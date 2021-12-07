edb3acd27bda5121bed399a84045dbca17590a49
duskmoon314 <duskmoon314@users.noreply.github.com>
2021-12-07 09:03:49 +0000

    deploy: 6d96e1365d8a0f3a0c507f93a213662987b9bdcd

--- index-3c0d4dfb6d9e1201.js
+++ index-3c0d4dfb6d9e1201.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-dc17247c82d10edf_bg.wasm', import.meta.url);
+        input = new URL('index-3c0d4dfb6d9e1201_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
