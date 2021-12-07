8e988b9e82f8b4317dd58e4e72715366e735291d
Campbell He <campbell.he@icloud.com>
2021-12-07 10:21:06 +0800

    feat: a playable tic-tac-toe implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Yew Tic-Tac-Toe
+
+Tic-Tac-Toe implemented with yew and similar to React version
+
+Ref: [React.js Tutorial](https://reactjs.org/tutorial/tutorial.html)
---
+++ src/main.rs
@@ -0,0 +1,187 @@
+use std::fmt::Display;
+use yew::prelude::*;
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+enum SquareValue {
+    X,
+    O,
+    None,
+}
+
+impl Display for SquareValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::X => write!(f, "X"),
+            Self::O => write!(f, "O"),
+            Self::None => write!(f, ""),
+        }
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct SquareProps {
+    value: SquareValue,
+    on_click: Callback<()>,
+}
+
+#[function_component(Square)]
+fn square(SquareProps { value, on_click }: &SquareProps) -> Html {
+    let on_click = {
+        let on_click = on_click.clone();
+        Callback::from(move |_| on_click.emit(()))
+    };
+    html! {
+        <button class="square" onclick={on_click}>{ value }</button>
+    }
+}
+
+#[derive(Properties)]
+struct BoardProps {
+    squares: [SquareValue; 9],
+    on_click: Callback<usize>,
+}
+
+impl PartialEq for BoardProps {
+    fn eq(&self, other: &Self) -> bool {
+        self.squares == other.squares
+    }
+}
+
+#[function_component(Board)]
+fn board(BoardProps { squares, on_click }: &BoardProps) -> Html {
+    let render_square = |i: usize| {
+        let on_click = {
+            let on_click = on_click.clone();
+            Callback::from(move |_| on_click.emit(i))
+        };
+        html! {
+            <Square value={squares[i]} on_click={on_click}/>
+        }
+    };
+
+    html! {
+        <div>
+            <div class="board-row">
+                { render_square(0) }
+                { render_square(1) }
+                { render_square(2) }
+            </div>
+            <div class="board-row">
+                { render_square(3) }
+                { render_square(4) }
+                { render_square(5) }
+            </div>
+            <div class="board-row">
+                { render_square(6) }
+                { render_square(7) }
+                { render_square(8) }
+            </div>
+        </div>
+    }
+}
+
+#[function_component(TicTacToe)]
+fn tictactoe() -> Html {
+    let history = use_state_eq(|| vec![[SquareValue::None; 9]]);
+    let step = use_state_eq(|| 0);
+    let winner = use_state_eq(|| SquareValue::None);
+
+    fn calculate_winner(squares: [SquareValue; 9]) -> SquareValue {
+        let lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for line in lines {
+            let [a, b, c] = line;
+            if squares[a] != SquareValue::None
+                && squares[a] == squares[b]
+                && squares[a] == squares[c]
+            {
+                return squares[a];
+            }
+        }
+
+        SquareValue::None
+    }
+
+    {
+        let history_dep = history.clone();
+        let history = history.clone();
+        let step_dep = step.clone();
+        let step = step.clone();
+        let winner = winner.clone();
+
+        use_effect_with_deps(
+            move |_| {
+                let current = history[*step];
+                winner.set(calculate_winner(current));
+
+                || ()
+            },
+            (history_dep, step_dep),
+        );
+    }
+
+    let handle_click = {
+        let history = history.clone();
+        let step = step.clone();
+        let winner = winner.clone();
+        Callback::from(move |i: usize| {
+            let mut new_history = (*history).clone();
+            new_history.truncate(*step + 1);
+            let mut current = new_history[*step];
+            if *winner != SquareValue::None || current[i] != SquareValue::None {
+                return;
+            }
+
+            current[i] = if *step % 2 == 0 {
+                SquareValue::X
+            } else {
+                SquareValue::O
+            };
+
+            new_history.append(&mut Vec::from([current]));
+            history.set(new_history);
+            step.set(*step + 1);
+        })
+    };
+
+    let undo = {
+        let step = step.clone();
+        let winner = winner.clone();
+        Callback::from(move |_| {
+            if *step > 0 {
+                step.set(*step - 1);
+                winner.set(SquareValue::None);
+            }
+        })
+    };
+
+    html! {
+        <main class="game">
+            <div class="game-board">
+                <Board squares={history[*step]} on_click={handle_click}/>
+            </div>
+            <div class="game-info">
+                <div>
+                    {match *winner {
+                        SquareValue::None => format!("Next player {}", if *step % 2 == 0 {"X"} else {"O"}),
+                        w => format!("Winner {}", w)
+                    }}
+                </div>
+                <button onclick={undo}> { "UNDO" } </button>
+            </div>
+        </main>
+    }
+}
+
+fn main() {
+    yew::start_app::<TicTacToe>();
+}
